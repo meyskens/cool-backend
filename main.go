08ad74c1826d39e3e98a4435e067857c1105e637
Maartje Eyskens <meyskens@me.com>
2017-10-20 17:10:20 +0200

    use def http

--- main.go
+++ main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/labstack/echo"
 	"google.golang.org/appengine"
 )
 
-var e *echo.Echo
-
 func main() {
-	e = echo.New()
-
-	e.GET("/", serveRoot)
-
-	// pass off app engine to echo
-	http.Handle("/", e)
+	http.HandleFunc("/", helloHandler)
 	appengine.Main()
 }
 
-func serveRoot(c echo.Context) error {
-	return c.String(http.StatusOK, "The coolest 200 OK you've ever seen!")
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Coolest 200 OK you've ever seen")
 }
