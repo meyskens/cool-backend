4a464917c4e401fadf27ebfd052cc5678d68ca8c
Maartje Eyskens <meyskens@me.com>
2017-12-04 20:19:12 +0100

    Add sigfox callback

--- main.go
+++ main.go
@@ -5,13 +5,21 @@ import (
 	"net/http"
 
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/sigfox/callback", handleCallback)
 	appengine.Main()
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Coolest 200 OK you've ever seen")
+}
+
+func interalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	ctx := appengine.NewContext(r)
+	log.Debugf(ctx, "Internal server error: %v", err)
+	http.Error(w, "Internal server error.", http.StatusInternalServerError)
 }
